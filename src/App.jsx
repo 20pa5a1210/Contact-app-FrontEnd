d9450df379c1ce4c8d00306db8406becb54a82e3
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2022-09-04 19:47:44 +0530

    error page added

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/addcontact" element={<NewContacts />} />
           <Route path="/contacts" element={<ViewContacts />} />
-          <Route path="/*" element={<ErrorPage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
     </store.Provider>
